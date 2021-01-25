7d87d99cfbaae6a48ed0f15301d97da796d239a4
zhangyi <1337390068@qq.com>
2021-01-26 00:01:41 +0800

    第二次

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,22 @@
 // 会与公共的配置文件进行合并
+const path = require('path');
 module.export={
-  configwebpack:{
+  configurewebpack:{
     resolve:{
       alias:{
         // '@':'src',
-        'assets':'@/assets',
-        'common':'@/common',
-        'components':'@/components',
-        'network':'@/network',
-        'views':'@/views'
+        // 'assets':'@/assets',
+        // 'common':'@/common',
+        // 'components':'@/components',
+        // 'network':'@/network',
+        // 'views':'@/views'
+        '@': path.resolve(__dirname, 'src'),
+        assets: path.resolve(__dirname, 'src/assets'),
+        common: path.resolve(__dirname, 'src/common'),
+        components: path.resolve(__dirname, 'src/components'),
+        network: path.resolve(__dirname, 'src/network'),
+        views: path.resolve(__dirname, 'src/views')
       }
     }
   }
-
-
 }

5263d5ef4f97024617991ca06cd92ce581d214c1
zhangyi <1337390068@qq.com>
2021-01-26 00:44:20 +0800

    文件别名修改

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,14 @@
 // 会与公共的配置文件进行合并
-const path = require('path');
-module.export={
-  configurewebpack:{
+module.exports={
+  configureWebpack:{
     resolve:{
       alias:{
         // '@':'src',
-        // 'assets':'@/assets',
-        // 'common':'@/common',
-        // 'components':'@/components',
-        // 'network':'@/network',
-        // 'views':'@/views'
-        '@': path.resolve(__dirname, 'src'),
-        assets: path.resolve(__dirname, 'src/assets'),
-        common: path.resolve(__dirname, 'src/common'),
-        components: path.resolve(__dirname, 'src/components'),
-        network: path.resolve(__dirname, 'src/network'),
-        views: path.resolve(__dirname, 'src/views')
+        'assets':'@/assets',
+        'common':'@/common',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views'
       }
     }
   }
